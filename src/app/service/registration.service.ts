ffdb9dcd5e1c9d9d03225e071aeeb04cdbc57845
Zurab <101800477+wedz0zur@users.noreply.github.com>
2025-04-23 12:20:06 +0300

    Update registration.service.ts

--- src/app/service/registration.service.ts
+++ src/app/service/registration.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable({
@@ -5,6 +6,13 @@ import { Injectable } from "@angular/core";
 })
 export class RegistrationService {
   users: any = []
+  
+  constructor(private http: HttpClient) {}
+  registreUser(formData: FormData){
+    return this.http.post("http://localhost:5000/auth/registration", formData)
+  }
+
+
   getUsers(){
     const storedUsers = localStorage.getItem('users');
     if (storedUsers) {
@@ -19,5 +27,4 @@ export class RegistrationService {
     this.users.push(newUser);
     localStorage.setItem("users", JSON.stringify(this.users));
   }
-  constructor() {}
 }
