f88991340495e0403c7fc15f2727b96c0537a6d3
Zurab <101800477+wedz0zur@users.noreply.github.com>
2025-04-23 11:29:27 +0300

    Update registration.component.ts

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,126 +1,108 @@
-import { Component, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RegistrationService } from '../service/registration.service';
-import { NgIf } from '@angular/common';
-import { CommonModule } from '@angular/common';
-
+import { Component, inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { RegistrationService } from "../service/registration.service";
+import { NgIf } from "@angular/common";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-registration',
+  selector: "app-registration",
   standalone: true,
   imports: [FormsModule, NgIf, CommonModule],
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"],
 })
 export class RegistrationComponent {
-  name:string = "";
+  name: string = "";
   userName: string = "";
   userAge: number | null = null;
   userEmail: string = "";
   userPassword: string = "";
   userCheckPassword: string = "";
-  userAvatar: string = "https://avatars.mds.yandex.net/i?id=93932abfd430c7aab32a3d45806ea6e6d4d0523a-4944707-images-thumbs&n=13";
+  userAvatar: string =
+    "https://avatars.mds.yandex.net/i?id=93932abfd430c7aab32a3d45806ea6e6d4d0523a-4944707-images-thumbs&n=13";
   error: string = "";
+  errorValid: string = "";
+  errorPassword: string = "";
   isSubmitting: boolean = false;
   showSuccessMessage: boolean = false;
 
   constructor(private registrationService: RegistrationService) {}
 
-  get isFormValid(): boolean {
-    return !!this.userName && 
-           this.userAge !== null && 
-           this.userAge > 0 && 
-           this.userPassword.length >= 6 && 
-           this.userPassword === this.userCheckPassword;
+  onFormSubmit(formData: any) {
+    const formDataToSend = new FormData();
+    formDataToSend.append("name", formData.name);
+    formDataToSend.append("username", formData.userName);
+    formDataToSend.append("age", formData.userAge.toString());
+    formDataToSend.append("email", formData.userEmail);
+    formDataToSend.append("password", formData.userPassword);
+
+    if(formData.userAvatar instanceof File){
+      formDataToSend.append("avatar", formData.userAvatar);
+    }
   }
 
-  get passwordMismatch(): boolean {
-    return this.userPassword !== this.userCheckPassword && 
-           this.userCheckPassword.length > 0;
-  }
-
-  get isPasswordWeak(): boolean {
-    return this.userPassword.length > 0 && this.userPassword.length < 6;
-  }
   http = inject(HttpClient);
-  addUser(): void {
-    if (!this.isFormValid) {
-      this.error = "Пожалуйста, заполните все поля корректно";
+
+  authorization() {
+    this.errorValid = "";
+    this.errorPassword = "";
+    this.error = "";
+
+    if (
+      !this.name ||
+      !this.userName ||
+      !this.userEmail ||
+      !this.userAge ||
+      !this.userPassword ||
+      !this.userCheckPassword
+    ) {
+      this.errorValid = "Пожалуйста, заполните все поля корректно";
+      return;
+    }
+    if (this.userAge <= 0) {
+      this.errorValid = "Возраст должен быть положительным числом";
+      return;
+    }
+    if (this.userPassword !== this.userCheckPassword) {
+      this.errorPassword = "Пароли должны совпадать";
+      return;
+    }
+    if (this.userPassword.length < 6) {
+      this.errorPassword = "Пароль должен быть не менее 6 символов";
       return;
     }
 
     this.isSubmitting = true;
-    this.error = "";
 
-    try {
-      const newUser = {
-        id: Date.now(),
-        name: this.userName,
-        age: this.userAge,
-        avatar: this.userAvatar,
-        password: this.userPassword
-      };
-
-      this.registrationService.addUser(newUser);
-      this.showSuccessMessage = true;
-      
-      setTimeout(() => {
-        this.resetForm();
-        this.isSubmitting = false;
-      }, 2000);
-      
-    } catch (err) {
-      this.error = "Произошла ошибка при регистрации";
-      this.isSubmitting = false;
-    }
-  }
-
-  private resetForm(): void {
-    this.userName = "";
-    this.userAge = null;
-    this.userPassword = "";
-    this.userCheckPassword = "";
-    this.userAvatar = "https://avatars.mds.yandex.net/i?id=93932abfd430c7aab32a3d45806ea6e6d4d0523a-4944707-images-thumbs&n=13";
-    this.showSuccessMessage = false;
-  }
-
-  onAvatarChange(): void {
-    if (this.userAvatar && !this.userAvatar.startsWith('http')) {
-      this.error = "Пожалуйста, введите корректный URL изображения";
-    } else {
-      this.error = "";
-    }
-  }
-
-  authorization() {
     const user = {
       name: this.name,
       username: this.userName,
       age: this.userAge,
       email: this.userEmail,
       password: this.userPassword,
+      avatar: this.userAvatar,
     };
+    this.isSubmitting = false;
 
-    this.http
-    .post('http://localhost:5000/auth/registration', user)
-    .subscribe({
+    this.http.post("http://localhost:5000/auth/registration", user).subscribe({
       next: (res: any) => {
-        if (res === 'Регистрация прошла успешно') {
+        if (res === "Регистрация прошла успешно") {
           this.showSuccessMessage = true;
-          setTimeout(() => this.resetForm(), 2000);
         } else {
-          this.error = 'Не удалось зарегистрировать пользователя';
+          this.error = "Не удалось зарегистрировать пользователя";
         }
       },
-      error: (err) => {
-        this.error = err.error?.message || 'Ошибка регистрации';
-        console.error('HTTP error:', err);
+      error: (e) => {
+        this.error = e.error?.message || "Ошибка регистрации";
+        console.error("HTTP error:", e);
       },
-      complete: () => {
-        this.isSubmitting = false;
-      }
     });
   }
 
+  uploadFile(file: any) {
+    console.log(file);
+  }
+
+  registration() {}
 }
